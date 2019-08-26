77ce9f99e3c48d3b9ae316a6b6ca577f51c94352
marcelvenegas <marcel.venega@gmail.com>
2019-08-26 23:44:26 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
 
-import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
+var a =["the","our"];
+var b =["great","big"];
+var c =["jogger","racoon"];
+var d =[".com"]
+for (var i= 2; i < d.length;i++){
+    for (var j= 0; j < a.length;j++)
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+}
